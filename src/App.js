257ddf8864585f9e4f27a3eb80f90e5daf716e55
sambhav2410 <73751899+sambhav2410@users.noreply.github.com>
2022-05-02 23:26:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ChessBoard from './ChessBoard';
 
 function App() {
   return (
-    <div style={{backgroundColor: "grey" , textAlign:"center" , fontFamily:"cursive"}}>
+    <div style={{backgroundColor: "black" ,color:"white", textAlign:"center" , fontFamily:"cursive"}}>
       <h2 style={{fontWeight : "500"}}>Choose a current position for knight and it will find all the possible places where the knight can move!!</h2>
       <ChessBoard />
     </div>
